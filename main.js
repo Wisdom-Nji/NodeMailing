b2cc658b2b520e1643f18e03260104e97d0576b8
Sherlock Wisdom <wisdomnji@gmail.com>
2019-12-15 13:46:20 +0000

    first template

---
+++ main.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+
+var sendingEmail = "sample_sending_email@gmail.com";
+var sendingEmailPassword = "sendingEmailPassword";
+var receivingEmail = "sample_receiving_email@mail.com";
+var mailSubject = "Hello world";
+var text = "Hello world";
+
+
+var transporter = nodemailer.createTransport({
+	server: mailingServer,
+	auth: {
+		user : sendingEmail,
+		pass : sendingEmailPassword
+	}
+});
+
+
+var mailOptions = {
+	from : sendingEmail,
+	to : receivingEmails,
+	subject : mailSubject,
+	text : text
+};
+
+transporter.sendMail( mailOptions, function(error, info) {
+	if( error) {
+		console.log( error );
+	}
+	else {
+		console.log("Email sent: " + info.response);
+	}
+});
